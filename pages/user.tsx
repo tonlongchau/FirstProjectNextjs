eeed7a8227b48fe711eedce06317ba947680d95e
tonlongchau <tonlongchau@gmail.com>
2023-03-07 11:10:05 +0700

    first commit

---
+++ pages/user.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function User() {
+  return (
+    
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis totam quod ratione dicta ad libero labore 
+      adipisci officiis rem blanditiis consequuntur, accusamus tempore doloremque dolore, cum quae quam voluptas sapiente!</p>
+  )
+}
